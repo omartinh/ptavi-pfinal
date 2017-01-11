4f0115f38c9439ddb19f8773e83f5a3eb250b43b
Oscar Martin Herrera <omartin@alpha11.aulas.gsyc.urjc.es>
2017-01-11 13:38:28 +0100

    client pep8 pasado

--- uaclient.py
+++ uaclient.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-#-*- coding: utf-8 -*-
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import socket
 import sys
@@ -20,28 +20,23 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
 
-
         self.lista = []
-        self.dicc = {"account" : ["username","passwd"],
-                    "uaserver" : ["ip","puerto"],
-                    "rtpaudio" : ["puerto"],
-                    "regproxy" : ["ip","puerto"],
-                    "log" : ["path"],
-                    "audio" : ["path"]}
-
+        self.dicc = {"account": ["username", "passwd"],
+                    "uaserver": ["ip", "puerto"],
+                    "rtpaudio": ["puerto"],
+                    "regproxy": ["ip", "puerto"],
+                    "log": ["path"],
+                    "audio": ["path"]}
 
     def startElement(self, name, attrs):
         if name in self.dicc:
-            empty={}
+            empty = {}
             for atrib in self.dicc[name]:
-                empty[atrib] = attrs.get(atrib,"")
-            self.lista.append([name,empty])
-
+                empty[atrib] = attrs.get(atrib, "")
+            self.lista.append([name, empty])
 
     def get_tags(self):
         return self.lista
-
-
 
 try:
     CONFIG = sys.argv[1]
@@ -50,15 +45,11 @@ try:
 except:
     sys.exit("Usage: python uaclient.py config method option")
 
-
-
 parser = make_parser()
 cHandler = SmallSMILHandler()
 parser.setContentHandler(cHandler)
 parser.parse(open(CONFIG))
-data=cHandler.get_tags()
-#print(data)
-
+data = cHandler.get_tags()
 
 #  Lista de diccionarios de nuestra lista--->"data"
 account = data[0][1]
@@ -73,7 +64,7 @@ audio = data[5][1]
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((regproxy['ip'],int(regproxy['puerto'])))
+my_socket.connect((regproxy['ip'], int(regproxy['puerto'])))
 
 #  Log
 event = 'Starting...'
@@ -92,14 +83,12 @@ if METHOD == 'REGISTER':
     my_socket.send(bytes(cabecera_r, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print(data.decode('utf-8'))
-    
 
     #  Log
     event = 'Send to ' + regproxy['ip'] + ':' + regproxy['puerto'] + ':'
     event += cabecera_r
     tiempo = time.time()
     AddLog(log['path'], tiempo, event)
-
 
     data_list = data.decode('utf-8').split()
     #  print(data_list)
@@ -121,7 +110,6 @@ if METHOD == 'REGISTER':
         event += linea
         tiempo = time.time()
         AddLog(log['path'], tiempo, event)
-        
 
 elif METHOD == 'INVITE':
     User = OPTION
@@ -129,10 +117,10 @@ elif METHOD == 'INVITE':
     linea2 = "Content-Type: application/sdp" + "\r\n"
     v = "v=0" + "\r\n"
     o = "o=" + account['username'] + " " + uaserver['ip'] + "\r\n"
-    s = "s=SesionGrove" + "\r\n" 
+    s = "s=SesionGrove" + "\r\n"
     t = "t=0" + "\r\n"
     m = "m=audio" + rtpaudio['puerto'] + " RTP" + "\r\n"
-    linea_sdp = v + o + s + t+ m
+    linea_sdp = v + o + s + t + m
     cabecera_i = "INVITE" + linea1 + linea2 + linea_sdp
 
     my_socket.send(bytes(cabecera_i, 'utf-8') + b'\r\n')
@@ -146,9 +134,7 @@ elif METHOD == 'INVITE':
     tiempo = time.time()
     AddLog(log['path'], tiempo, event)
 
-
-
-    if linea_r[2] == 'Trying' and linea_r[5] == 'Ring' and linea_r[8] == 'OK' :
+    if linea_r[2] == 'Trying' and linea_r[5] == 'Ring' and linea_r[8] == 'OK':
         User = OPTION
         linea = "ACK" + " sip:" + User + " SIP/2.0\r\n"
         my_socket.send(bytes(linea, 'utf-8') + b'\r\n')
@@ -178,5 +164,4 @@ elif METHOD == 'BYE':
     tiempo = time.time()
     AddLog(log['path'], tiempo, event)
 
-    
 my_socket.close()
